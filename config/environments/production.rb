addf6880aaa3a08287ca71e812744829ca93025f
Kylords <mikaylo_veloria@dlsu.edu.ph>
2024-01-15 13:42:19 +0800

    change actioncable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = "wss://pos-system-vue.vercel.app/cable"
-  config.action_cable.allowed_request_origins = [ "https://pos-system-vue.vercel.app/" ]
+  config.action_cable.allowed_request_origins = [ "https://pos-system-vue.vercel.app" ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
